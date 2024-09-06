c51f4d412a06afc033f4de774750da5d12fd6de3
kangsujung614 <sjkang614@gmail.com>
2024-09-06 23:46:44 +0900

    [JS] 선택된 셀렉트 박스 옵션 텍스트 콘솔에 출력

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 // 회의실 예약 정보 추가하는 함수(일단 콘솔에 출력)
 const addReservation= () => {
+    // 1. 선택한 회의실 용도(select)를 출력하는 부분
+    const use = document.querySelector('.use option:checked').innerText;
+    console.log(`회의 용도: ${use}`);
 
-    // 1. 선택한 회의(input[type="radio"])의 규모를 출력하는 부분
-    const allScale = document.querySelectorAll('.scale'); // 회의 규모 선택을 위한 input 요소들을 모두 선택하여 Nodelist로 할당
-    allScale.forEach(element => { // 코드 흐름 2번과 유사하므로 주석 생략
-        if(element.checked){
-            console.log(`회의 규모: ${element.parentNode.innerText}`);
-        }
-    });
+    // 2. 선택한 회의(input[type="radio"])의 규모를 출력하는 부분
+    const scale = document.querySelector('.scale:checked').parentNode.innerText;
+    console.log(`회의 규모: ${scale}`);
 
-    // 2. 선택한 장비(input[type="checkbox"])를 출력하는 부분 
+    // 3. 선택한 장비(input[type="checkbox"])를 출력하는 부분 
     const allEquipments = document.querySelectorAll('.equipment'); // 장비 선택을 위한 input 요소들을 모두 선택하여 Nodelist로 할당
     const checkedEquipments = []; // 선택한 장비를 담을 배열 선언
     allEquipments.forEach(element => { // Nodelist는 유사 배열이므로 배열 순회하는 forEach 함수 사용 가능
@@ -19,11 +18,11 @@ const addReservation= () => {
     });
     console.log(`필요 장비: ${checkedEquipments}`);
 
-    // 3. 예약자 성함(input[type="text"]) 요소의 현재 값을 가져오는 부분
+    // 4. 예약자 성함(input[type="text"]) 요소의 현재 값을 가져오는 부분
     const userName = document.getElementById('user-name').value;
     console.log(`예약자 성함: ${userName}`);
 
-    // 4. 회의 주제(textarea 요소)
+    // 5. 회의 주제(textarea 요소)
     const meetingTopic = document.getElementById('meeting-topic').value;
     console.log(`회의 주제: ${meetingTopic}`);
 }
