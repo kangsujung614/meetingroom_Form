7e7939367de5b53b56c2a1b9356017e7c92f88ae
kangsujung614 <sjkang614@gmail.com>
2024-09-06 22:43:42 +0900

    [JS] 체크된 라디오 버튼 요소 콘솔에 출력

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
 // 회의실 예약 정보 추가하는 함수(일단 콘솔에 출력)
 const addReservation= () => {
-    
-    // 1. 선택한 장비(input[type="checkbox"])를 출력하는 부분 
-    allEquipments = document.querySelectorAll('.equipment') // 장비 선택을 위한 input 요소들을 모두 선택하여 Nodelist로 할당됨
-    let checkedEquipments = [] // 선택한 장비를 담을 배열 선언
-    allEquipments.forEach(element => {
-        if(element.checked){ // 객체의 속성에 접근하기 위해 점 연산자를 사용한 문법: .checked 사용(.checked는 체크박스나 라디오 버튼의 선택 상태를 나타내는 불리언(boolean) 속성)
-            checkedEquipments.push(element.parentNode.innerText) // 장비 이름을 출력하기 위해 부모 요소(parentNode)안의 텍스트 값만(innerText) 배열에 추가(push)
+
+    // 1. 선택한 회의(input[type="radio"])의 규모를 출력하는 부분
+    const allScale = document.querySelectorAll('.scale'); // 회의 규모 선택을 위한 input 요소들을 모두 선택하여 Nodelist로 할당
+    allScale.forEach(element => { // 코드 흐름 2번과 유사하므로 주석 생략
+        if(element.checked){
+            console.log(`회의 규모: ${element.parentNode.innerText}`);
         }
-    })
-    console.log(`필요 장비: ${checkedEquipments}`)
+    });
 
-    // 2. 예약자 성함(input[type="text"]) 요소의 현재 값을 가져오는 부분
-    let userName = document.getElementById('user-name').value
-    console.log(`예약자 성함: ${userName}`)
+    // 2. 선택한 장비(input[type="checkbox"])를 출력하는 부분 
+    const allEquipments = document.querySelectorAll('.equipment'); // 장비 선택을 위한 input 요소들을 모두 선택하여 Nodelist로 할당
+    const checkedEquipments = []; // 선택한 장비를 담을 배열 선언
+    allEquipments.forEach(element => { // Nodelist는 유사 배열이므로 배열 순회하는 forEach 함수 사용 가능
+        if(element.checked){ // 객체의 속성에 접근하기 위해 점 연산자를 사용한 문법: .checked 사용(.checked는 체크박스나 라디오 버튼의 선택 상태를 나타내는 불리언(boolean) 속성)
+            checkedEquipments.push(element.parentNode.innerText); // 장비 이름을 출력하기 위해 부모 요소(parentNode)안의 텍스트 값만(innerText) 배열에 추가(push)
+        }
+    });
+    console.log(`필요 장비: ${checkedEquipments}`);
 
-    // 3. 회의 주제(textarea 요소)
-    let meetingTopic = document.getElementById('meeting-topic').value
-    console.log(`회의 주제: ${meetingTopic}`)
+    // 3. 예약자 성함(input[type="text"]) 요소의 현재 값을 가져오는 부분
+    const userName = document.getElementById('user-name').value;
+    console.log(`예약자 성함: ${userName}`);
+
+    // 4. 회의 주제(textarea 요소)
+    const meetingTopic = document.getElementById('meeting-topic').value;
+    console.log(`회의 주제: ${meetingTopic}`);
 }
